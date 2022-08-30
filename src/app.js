9bb4999897b06f1910f5964dbad89aedc5a55ae2
solisdari1999 <solisdari1999@gmail.com>
2022-08-30 20:29:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoExc = Math.round(Math.random() * who.length);
+  let actionExc = Math.round(Math.random() * action.length);
+  let whatExc = Math.round(Math.random() * what.length);
+  let whenExc = Math.round(Math.random() * when.length);
+
+  return (
+    who[whoExc] +
+    " " +
+    action[actionExc] +
+    " " +
+    what[whatExc] +
+    " " +
+    when[whenExc]
+  );
 };
